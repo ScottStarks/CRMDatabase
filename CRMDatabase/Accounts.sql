02f58985b1658ee0bd487f972742d3608f5eb5cb
Scott <Scott@DESKTOP-VUPFLSV>
2016-06-25 00:54:09 -0500

    Add project files.

---
+++ CRMDatabase/Accounts.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Accounts]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [FirstName] NCHAR(20) NULL, 
+    [LastName] NCHAR(20) NULL, 
+    [emailAddress] NCHAR(50) NULL, 
+    [Password] NCHAR(10) NULL
+)
